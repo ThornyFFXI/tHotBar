b3747967d5652b3e1714fb6b3b92b8408636ac18
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2023-07-27 06:54:45 -0400

    1.18 - Fix crash with cost function on command/ws

--- tHotBar.lua
+++ tHotBar.lua
@@ -21,7 +21,7 @@
 
 addon.name      = 'tHotBar';
 addon.author    = 'Thorny';
-addon.version   = '1.17';
+addon.version   = '1.18';
 addon.desc      = 'Displays macros as visible and clickable elements.';
 addon.link      = 'https://ashitaxi.com/';
 
